ecda20e31fef876caeb3a903c4c128458e785b5a
Kevin Hill <kevinkhill@gmail.com>
2015-12-12 08:52:18 -0800

    updating lava.js tests

--- javascript/lava.spec.js
+++ javascript/lava.spec.js
@@ -10,7 +10,7 @@ function MockChart() {
 
 describe('lava.js core functions', function() {
 
-  describe('lava.get()', function() {
+  describe('lava.getChart()', function() {
 
     beforeEach(function() {
       lava.charts = {
@@ -21,24 +21,24 @@ describe('lava.js core functions', function() {
     });
 
     it('should return a valid chart to the callback.', function() {
-      lava.get('TestChart', function (chart) {
+      lava.getChart('TestChart', function (chart) {
         expect(chart.prop).toEqual(1);
       });
     });
 
     it('should throw an error if the chart is not found.', function() {
-      expect(function () { lava.get('Bee Population', function(){}); })
-        .toThrow(new Error('[Lavacharts] Chart "Bee Population" was not found'));
+      expect(function () { lava.getChart('Bee Population', function(){}); })
+        .toThrow(new Error('[Lavacharts] Chart "Bee Population" was not found.'));
     });
 
     it('should throw an error if a string chart label is not given.', function() {
-      expect(function () { lava.get([], function(){}); })
-        .toThrow(new Error('[Lavacharts] The syntax for lava.get must be (str ChartLabel, fn Callback)'));
+      expect(function () { lava.getChart([], function(){}); })
+        .toThrow(new Error('[Lavacharts] object is not a valid chart label.'));
     });
 
     it('should throw an error if a function callback is not given.', function() {
-      expect(function () { lava.get('TestChart', {}); })
-        .toThrow(new Error('[Lavacharts] The syntax for lava.get must be (str ChartLabel, fn Callback)'));
+      expect(function () { lava.getChart('TestChart', {}); })
+        .toThrow(new Error('[Lavacharts] object is not a valid callback.'));
     });
 
   });
